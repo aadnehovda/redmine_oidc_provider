250fa1c522cc21449d429a451b64cb88c48c5539
Ådne Hovda <32843498+aadnehovda@users.noreply.github.com>
2026-05-24 11:19:23 +0200

    Default DCR scopes from Doorkeeper

--- lib/redmine_oidc_provider/dynamic_client_registration.rb
+++ lib/redmine_oidc_provider/dynamic_client_registration.rb
@@ -7,7 +7,7 @@ module RedmineOidcProvider
     def application_params(registration)
       super.tap do |attributes|
         attributes[:scopes] =
-          params[:scope].presence || RedmineOidcProvider::Config.dcr_default_scopes
+          params[:scope].presence || Doorkeeper.configuration.scopes.to_s
       end
     end
   end
